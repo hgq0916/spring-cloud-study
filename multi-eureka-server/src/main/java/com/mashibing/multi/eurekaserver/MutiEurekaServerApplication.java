f2aff26757b35459172b5257676e73a00dda9b74
hgq0916 <1286908118@qq.com>
2020-08-18 09:58:27 +0800

    注册中心集群搭建

---
+++ multi-eureka-server/src/main/java/com/mashibing/multi/eurekaserver/MutiEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.mashibing.multi.eurekaserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MutiEurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MutiEurekaServerApplication.class,args);
+  }
+
+}
