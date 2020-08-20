c0fe0f00a1c48874b7ddef6b51769842f2d84257
hgq0916 <1286908118@qq.com>
2020-08-20 21:49:16 +0800

    使用hystrix dashboard查看hystrix的运行状态

--- User-Consumer/src/main/java/com/mashibing/eureka/user/provider/UserConsumerApplication.java
+++ User-Consumer/src/main/java/com/mashibing/eureka/user/provider/UserConsumerApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//启用hstrix dashboard
+@EnableHystrixDashboard
 @EnableCircuitBreaker//启用hstrix
 @EnableFeignClients(basePackages = "com.mashibing.eureka.user.provider")
 @EnableEurekaClient
