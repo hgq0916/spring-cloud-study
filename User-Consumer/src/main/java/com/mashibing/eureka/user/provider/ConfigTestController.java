2d9829e9c2121828a42c17b9d5399ce35395a714
hgq0916 <1286908118@qq.com>
2020-08-21 20:48:37 +0800

    热更新单个微服务的配置

--- User-Consumer/src/main/java/com/mashibing/eureka/user/provider/ConfigTestController.java
+++ User-Consumer/src/main/java/com/mashibing/eureka/user/provider/ConfigTestController.java
@@ -4,11 +4,13 @@ import com.mashibing.user.api.User;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigTestController {
 
   @Value("${myconfig}")
