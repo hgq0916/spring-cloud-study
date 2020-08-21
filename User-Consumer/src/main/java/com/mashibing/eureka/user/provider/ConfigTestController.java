1fc4c66236fcfcae3e1d3c37f54aaf68d9fc7f37
hgq0916 <1286908118@qq.com>
2020-08-21 20:41:59 +0800

    微服务使用配置中心配置

---
+++ User-Consumer/src/main/java/com/mashibing/eureka/user/provider/ConfigTestController.java
@@ -0,0 +1,22 @@
+package com.mashibing.eureka.user.provider;
+
+import com.mashibing.user.api.User;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigTestController {
+
+  @Value("${myconfig}")
+  private String myconfig;
+
+  @GetMapping
+  public String config(){
+    return myconfig;
+  }
+
+}
